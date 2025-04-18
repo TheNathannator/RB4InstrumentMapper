21bebc0cda5b96c9fa74d0513a50a6c635438804
Nathan <nathanhurst03@gmail.com>
2025-04-18 14:26:41 -0600

    Remove building of standalone versions from publish script Not particularly worth it on second thought, there are additional dependencies the installer handles for you.

--- publish.bat
+++ publish.bat
@@ -13,15 +13,15 @@ echo.
 
 REM Build projects
 
-echo Building standalone CLI
-dotnet build RB4InstrumentMapper.CLI\RB4InstrumentMapper.CLI.csproj "-p:Configuration=Release;Platform=x64" -verbosity:minimal -consoleloggerparameters:NoSummary
-if %ERRORLEVEL% NEQ 0 goto error
-echo.
+REM echo Building standalone CLI
+REM dotnet build RB4InstrumentMapper.CLI\RB4InstrumentMapper.CLI.csproj "-p:Configuration=Release;Platform=x64" -verbosity:minimal -consoleloggerparameters:NoSummary
+REM if %ERRORLEVEL% NEQ 0 goto error
+REM echo.
 
-echo Building standalone GUI
-dotnet build RB4InstrumentMapper.GUI\RB4InstrumentMapper.GUI.csproj "-p:Configuration=Release;Platform=x64" -verbosity:minimal -consoleloggerparameters:NoSummary
-if %ERRORLEVEL% NEQ 0 goto error
-echo.
+REM echo Building standalone GUI
+REM dotnet build RB4InstrumentMapper.GUI\RB4InstrumentMapper.GUI.csproj "-p:Configuration=Release;Platform=x64" -verbosity:minimal -consoleloggerparameters:NoSummary
+REM if %ERRORLEVEL% NEQ 0 goto error
+REM echo.
 
 echo Building CLI+GUI installer
 dotnet build RB4InstrumentMapper.Installer\RB4InstrumentMapper.Installer.wixproj "-p:Configuration=Release;Platform=x64" -verbosity:minimal -consoleloggerparameters:NoSummary
@@ -30,19 +30,19 @@ echo.
 
 REM Copy files and build archives
 
-echo Packaging standalone CLI
-7z a "%~dp0\publish\RB4InstrumentMapper.CLI-x64.zip" "%~dp0\RB4InstrumentMapper.CLI\bin\x64\Release\net472" -bb3 -bd -sse > nul
-if %ERRORLEVEL% NEQ 0 goto error
-7z rn "%~dp0\publish\RB4InstrumentMapper.CLI-x64.zip" "net472" "RB4InstrumentMapper.CLI" -bb0 -bd > nul
-if %ERRORLEVEL% NEQ 0 goto error
-echo.
+REM echo Packaging standalone CLI
+REM 7z a "%~dp0\publish\RB4InstrumentMapper.CLI-x64.zip" "%~dp0\RB4InstrumentMapper.CLI\bin\x64\Release\net472" -bb3 -bd -sse > nul
+REM if %ERRORLEVEL% NEQ 0 goto error
+REM 7z rn "%~dp0\publish\RB4InstrumentMapper.CLI-x64.zip" "net472" "RB4InstrumentMapper.CLI" -bb0 -bd > nul
+REM if %ERRORLEVEL% NEQ 0 goto error
+REM echo.
 
-echo Packaging standalone GUI
-7z a "%~dp0\publish\RB4InstrumentMapper.GUI-x64.zip" "%~dp0\RB4InstrumentMapper.GUI\bin\x64\Release\net472" -bb3 -bd -sse > nul
-if %ERRORLEVEL% NEQ 0 goto error
-7z rn "%~dp0\publish\RB4InstrumentMapper.GUI-x64.zip" "net472" "RB4InstrumentMapper.GUI" -bb0 -bd > nul
-if %ERRORLEVEL% NEQ 0 goto error
-echo.
+REM echo Packaging standalone GUI
+REM 7z a "%~dp0\publish\RB4InstrumentMapper.GUI-x64.zip" "%~dp0\RB4InstrumentMapper.GUI\bin\x64\Release\net472" -bb3 -bd -sse > nul
+REM if %ERRORLEVEL% NEQ 0 goto error
+REM 7z rn "%~dp0\publish\RB4InstrumentMapper.GUI-x64.zip" "net472" "RB4InstrumentMapper.GUI" -bb0 -bd > nul
+REM if %ERRORLEVEL% NEQ 0 goto error
+REM echo.
 
 echo Copying installer package
 copy "%~dp0\RB4InstrumentMapper.Installer\bin\x64\Release\RB4InstrumentMapper.Installer.exe" "%~dp0\publish\RB4InstrumentMapperInstaller-x64.exe"
